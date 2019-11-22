44ac80367ae280c27dbb6af6913523ac2f28d0bf
DekiRostilj7 <dejan.rosulj89@gmail.com>
2019-11-22 20:28:07 +0100

    Second task finished

--- src/components/AppCustomer.vue
+++ src/components/AppCustomer.vue
@@ -1,11 +1,25 @@
 <template>
     <div>
         <h1>Customers</h1>
+            <p v-for="(customer, index) in customers" :key="index" >
+                {{ customer.fullName}} {{customer.email}} <button @click="handleRemove(customer)">Delete</button>
+            </p>
     </div>
 </template>
 
 <script>
+import { customerService } from "../services/CustomerService"
 export default {
-    
+    data(){
+        return {
+            customers: customerService.list()
+        }
+    },
+
+    methods: {
+        handleRemove(customer){
+            customerService.remove(customer);
+        }
+    }
 }
 </script>
