11345fcd52ada18d53fae7ec58aaf16ee1015f49
DekiRostilj7 <dejan.rosulj89@gmail.com>
2019-11-22 19:51:15 +0100

    First task finished

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Welcome to the quick e-market!</h1>
+    <nav>
+      <ul>
+        <li>
+          <router-link to="/customers">Customers</router-link>
+        </li>
+        <li>
+          <router-link to="/products">Products</router-link>
+        </li>
+      </ul>
+    </nav>  
+    <router-view /> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
 
@@ -24,5 +30,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.link-active {
+  color: red;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+
+import AppCustomer from './components/AppCustomer.vue';
+import AppProduct from './components/AppProduct.vue';
+
+const routes = [
+  {name: "Customers", path: "/customers", component: AppCustomer},
+  {name: "Products", path: "/products", component: AppProduct}
+]
+
+const router = new VueRouter({
+routes,
+linkExactActiveClass: "link-active",
+mode: "history"
+});
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
